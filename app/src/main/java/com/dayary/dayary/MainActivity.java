ea2be96037e74dab703d73b54d18d09839bb372a
Pio <70201882+pjs990301@users.noreply.github.com>
2022-05-02 13:25:25 +0900

    Main 변경사항 -> ID/PW 입력창 크기 맞추기

--- app/src/main/java/com/dayary/dayary/MainActivity.java
+++ app/src/main/java/com/dayary/dayary/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
