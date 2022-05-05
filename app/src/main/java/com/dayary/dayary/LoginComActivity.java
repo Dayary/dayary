5271aa00f10a138ba28b9b7f0c50368032f155b4
Seungil <thesence01@naver.com>
2022-05-05 22:56:54 +0900

    commit han homepage.xml, login_register.xml

--- app/src/main/java/com/dayary/dayary/LoginComActivity.java
+++ app/src/main/java/com/dayary/dayary/LoginComActivity.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginComActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+
 }
