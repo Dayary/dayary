bbced8bccc5584cdcd9b908a213f482a792a1435
Pio <70201882+pjs990301@users.noreply.github.com>
2022-05-06 16:32:06 +0900

    count num 자동 변경

--- app/src/main/java/com/dayary/dayary/HomeActivity.java
+++ app/src/main/java/com/dayary/dayary/HomeActivity.java
@@ -7,17 +7,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Button btn_pen;
+    private TextView countView;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,27 @@ public class HomeActivity extends AppCompatActivity {
         System.out.println(postModel.getUserId());
         mAuth = FirebaseAuth.getInstance();
         final FirebaseUser user = mAuth.getCurrentUser();
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("user").child(postModel.userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    if(dataSnapshot != null) {
+                        count++;
+                    }
+                }
+                String countValue = Integer.toString(count);
+                countView = findViewById(R.id.num_memories);
+                countView.setText(countValue);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         btn_pen = (Button) findViewById(R.id.icons8_penc);
         btn_pen.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +84,6 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-
 
 
 }
