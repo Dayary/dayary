60ee969c346375afe06aa3745cd85342b9b18903
Pio <70201882+pjs990301@users.noreply.github.com>
2022-05-10 21:45:39 +0900

    취소 수정

--- app/src/main/java/com/dayary/dayary/PopupActivity.java
+++ app/src/main/java/com/dayary/dayary/PopupActivity.java
@@ -61,15 +61,17 @@ public class PopupActivity extends Activity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         //바깥레이어 클릭시 안닫히게
-        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+        /*if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
             return false;
         }
+        */
+
         return true;
     }
     @Override
     public void onBackPressed() {
         //안드로이드 백버튼 막기
-        return;
+        //return;
     }
     @RequiresApi(api = Build.VERSION_CODES.O)
     public String getTodayDate() {
