bf9340945daa1f7b3f248623153b6477780d4d32
Pio <70201882+pjs990301@users.noreply.github.com>
2022-05-12 14:12:37 +0900

    캘린더 하단바 버튼 수정

--- app/src/main/java/com/dayary/dayary/calendarActivity.java
+++ app/src/main/java/com/dayary/dayary/calendarActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.icu.text.Edits;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.style.ForegroundColorSpan;
@@ -16,6 +17,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -37,6 +39,7 @@ import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
@@ -49,11 +52,16 @@ import java.util.concurrent.Executors;
 
 public class calendarActivity extends AppCompatActivity {
 
+    private View btn_home;
     private View btn_pen;
     private View btn_loc;
 
+
+    private String todayDate;
+    private String lastDate;
+
+    private Query query;
     private PostModel postModel;
-    private Query query0;
     ProgressDialog dialog;
 
     private MaterialCalendarView calendarView;
@@ -81,7 +89,62 @@ public class calendarActivity extends AppCompatActivity {
         calendarView.setDateTextAppearance(R.style.CustomDateTextAppearance);
         new ApiSimulator(result).executeOnExecutor(Executors.newSingleThreadExecutor());
 
+
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        query = database.child("user").child(postModel.userId).limitToLast(1);
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    if (dataSnapshot != null) {
+                        String returnValue = snapshot.getValue().toString();
+                        System.out.println(returnValue);
+                        int idx = returnValue.indexOf("=");
+                        lastDate = returnValue.substring(1, idx);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //하단 버튼 이동
+        //홈으로 이동
+        btn_home = findViewById(R.id.icons8_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                intent.putExtra("model", (Serializable) postModel);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //글쓰기 이동
+        btn_pen = findViewById(R.id.icons8_penc);
+        btn_pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnPopupClick(view);
+            }
+        });
+        //Map 이동
+        btn_loc = findViewById(R.id.icons8_loca);
+        btn_loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), mapActivity.class);
+                intent.putExtra("model", (Serializable) postModel);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
 
     private class ApiSimulator extends AsyncTask<Void, Void, List<CalendarDay>> {
 
@@ -129,6 +192,81 @@ public class calendarActivity extends AppCompatActivity {
             calendarView.addDecorator(new EventDecorator(Color.parseColor("#62A60C"), calendarDays));
         }
     }
+
+    public void mOnPopupClick(View v) {
+        Intent intent = new Intent(this, PopupActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String getTodayDate() {
+        //로컬 디바이스의 날짜를 가져옴
+        LocalDate todaysDate = LocalDate.now();
+        int curday = todaysDate.getDayOfWeek().getValue();
+        String CurDate = "";
+        switch (curday) {
+            case 1:
+                CurDate = todaysDate + "-" + "Mon";
+                break;
+            case 2:
+                CurDate = todaysDate + "-" + "Tue";
+                break;
+            case 3:
+                CurDate = todaysDate + "-" + "Wed";
+                break;
+            case 4:
+                CurDate = todaysDate + "-" + "Thur";
+                break;
+            case 5:
+                CurDate = todaysDate + "-" + "Fri";
+                break;
+            case 6:
+                CurDate = todaysDate + "-" + "Sat";
+                break;
+            case 7:
+                CurDate = todaysDate + "-" + "Sun";
+                break;
+        }
+
+        return CurDate;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println(requestCode);
+        System.out.println(resultCode);
+        todayDate = getTodayDate();
+        Intent intent = null;
+        if (requestCode == 1) {
+            if (resultCode == 0) {
+                if (todayDate.equals(lastDate)) {
+                    intent = new Intent(getApplicationContext(), corDel.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                    Toast.makeText(calendarActivity.this, "작성한 글이 있습니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    intent = new Intent(getApplicationContext(), normalWrite.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                }
+                startActivity(intent);
+                finish();
+            } else if (resultCode == 1) {
+                if (todayDate.equals(lastDate)) {
+                    intent = new Intent(getApplicationContext(), question_corDel.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                    Toast.makeText(calendarActivity.this, "작성한 글이 있습니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    intent = new Intent(getApplicationContext(), writequestion.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                }
+                startActivity(intent);
+                finish();
+            } else {
+
+            }
+        }
+    }
 }
 
 class EventDecorator implements DayViewDecorator {
@@ -151,7 +289,8 @@ class EventDecorator implements DayViewDecorator {
         view.addSpan(new DotSpan(8, color)); // 날자밑에 점
     }
 }
-class SaturdayDecorator implements DayViewDecorator{
+
+class SaturdayDecorator implements DayViewDecorator {
     private final Calendar calendar = Calendar.getInstance();
 
     public SaturdayDecorator() {
@@ -169,7 +308,8 @@ class SaturdayDecorator implements DayViewDecorator{
         view.addSpan(new ForegroundColorSpan(Color.BLUE));
     }
 }
-class SundayDecorator implements DayViewDecorator{
+
+class SundayDecorator implements DayViewDecorator {
     private final Calendar calendar = Calendar.getInstance();
 
     public SundayDecorator() {
@@ -187,7 +327,8 @@ class SundayDecorator implements DayViewDecorator{
         view.addSpan(new ForegroundColorSpan(Color.RED));
     }
 }
-class oneDayDecorator implements DayViewDecorator{
+
+class oneDayDecorator implements DayViewDecorator {
     private CalendarDay date = CalendarDay.today();
 
     @Override
