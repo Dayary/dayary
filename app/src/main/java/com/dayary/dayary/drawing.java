5afc6b0e6e00fef6b37074641c7eaaca28f3a175
Pio <70201882+pjs990301@users.noreply.github.com>
2022-05-23 21:44:02 +0900

    업로드 작업 수정

--- app/src/main/java/com/dayary/dayary/drawing.java
+++ app/src/main/java/com/dayary/dayary/drawing.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,11 +18,24 @@ import android.widget.Toast;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.io.ByteArrayOutputStream;
+import java.io.Serializable;
+import java.time.LocalDate;
 
 public class drawing extends AppCompatActivity {
     drawingView view;
@@ -30,10 +45,26 @@ public class drawing extends AppCompatActivity {
     Resources res;
     Bitmap bitmap;
 
+    private PostModel postModel;
+    private String todayDate;
+    private String lastDate = "";
+    private Query query1;
+
+    private View btn_home;
+    private View btn_pen;
+    private View btn_loc;
+    private View btn_cal;
+    private View btn_list;
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_draw);
+        setContentView(R.layout.activity_drawingboard1);
+        Intent intent = getIntent();
+        postModel = (PostModel) intent.getSerializableExtra("model");
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
         view = new drawingView(this);
 
         container = findViewById(R.id.container);
@@ -42,10 +73,11 @@ public class drawing extends AppCompatActivity {
         params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
         container.addView(view, params);
 
-        Button btn = findViewById(R.id.colorPickerButton);
-        Button btn2 = findViewById(R.id.thickPickerButton);
-        Button btn3 = findViewById(R.id.eraserButton);
-        Button btn4 = findViewById(R.id.saveButton);
+        View btn = findViewById(R.id.icons8_palette);
+        View btn2 = findViewById(R.id.icons8_line);
+        View btn3 = findViewById(R.id.icons8_eras);
+        View btn4 = findViewById(R.id.icons8_save);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +106,100 @@ public class drawing extends AppCompatActivity {
                 intent.putExtra("image",bitmap);
                 setResult(200, intent);
                 finish();
+            }
+        });
+        try {
+            query1 = database.child("user").child(postModel.userId).limitToLast(1);
+            query1.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        if (dataSnapshot != null) {
+                            String returnValue = snapshot.getValue().toString();
+                            int idx = returnValue.indexOf("=");
+                            lastDate = returnValue.substring(1, idx);
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }catch (Exception e){
+            System.out.println();
+        }
+        //하단 버튼 이동
+        //홈으로 이동
+        btn_home = findViewById(R.id.icons8_home);
+        btn_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                intent.putExtra("model", (Serializable) postModel);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //리스트 이동
+        btn_list = findViewById(R.id.icons8_jour);
+        btn_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), listActivity.class);
+                intent.putExtra("model", (Serializable) postModel);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //글쓰기 이동
+        btn_pen = findViewById(R.id.icons8_penc);
+        btn_pen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mOnPopupClick(view);
+            }
+        });
+        //Map 이동
+        btn_loc = findViewById(R.id.icons8_loca);
+        btn_loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), mapActivity.class);
+                intent.putExtra("model", (Serializable) postModel);
+                startActivity(intent);
+                finish();
+            }
+        });
+        // 캘린더 이동
+        btn_cal = findViewById(R.id.icons8_cale);
+        btn_cal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+                database.child("user").child(postModel.userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                        if (!task.isSuccessful()) {
+                            Log.e("firebase", "Error getting data", task.getException());
+                        } else {
+                            String[] value = task.getResult().getValue().toString().split("\\}\\}, ");
+                            value[0] = value[0].substring(1);
+                            for (int i = 0; i < value.length; i++) {
+                                value[i] = value[i].substring(0, 10);
+                            }
+                            Intent intent = new Intent(getApplicationContext(), calendarActivity.class);
+                            intent.putExtra("cal", value);
+                            intent.putExtra("model", (Serializable) postModel);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                });
+
             }
         });
 
@@ -126,5 +252,76 @@ public class drawing extends AppCompatActivity {
         });
         colorPicker.show();
     }
+    public void mOnPopupClick(View v) {
+        Intent intent = new Intent(this, PopupActivity.class);
+        startActivityForResult(intent, 1);
+    }
+    //글쓰기 Pop창 선택 onActivityResult
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        todayDate = getTodayDate();
+        Intent intent = null;
+        if (requestCode == 1) {
+            if (resultCode == 0) {
+                if (todayDate.equals(lastDate)) {
+                    intent = new Intent(getApplicationContext(), corDel.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                    Toast.makeText(drawing.this, "작성한 글이 있습니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    intent = new Intent(getApplicationContext(), normalWrite.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                }
+                startActivity(intent);
+            } else if (resultCode == 1) {
+                if (todayDate.equals(lastDate)) {
+                    intent = new Intent(getApplicationContext(), question_corDel.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                    Toast.makeText(drawing.this, "작성한 글이 있습니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    intent = new Intent(getApplicationContext(), writequestion.class);
+                    intent.putExtra("model", (Serializable) postModel);
+                }
+                startActivity(intent);
+            } else {
+
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public String getTodayDate() {
+        //로컬 디바이스의 날짜를 가져옴
+        LocalDate todaysDate = LocalDate.now();
+        int curday = todaysDate.getDayOfWeek().getValue();
+        String day = "";
+        switch (curday) {
+            case 1:
+                day = todaysDate + "-" + "Mon";
+                break;
+            case 2:
+                day = todaysDate + "-" + "Tue";
+                break;
+            case 3:
+                day = todaysDate + "-" + "Wed";
+                break;
+            case 4:
+                day = todaysDate + "-" + "Thur";
+                break;
+            case 5:
+                day = todaysDate + "-" + "Fri";
+                break;
+            case 6:
+                day = todaysDate + "-" + "Sat";
+                break;
+            case 7:
+                day = todaysDate + "-" + "Sun";
+                break;
+        }
+
+        return day;
+    }
+
 
 }
